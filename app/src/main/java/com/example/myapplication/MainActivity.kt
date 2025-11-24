410bee09f472272055c24d341fe036220380b3b6
mrgoodkit <mrgoodkit.db@gmail.com>
2025-11-24 12:38:20 +0300

    created new view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,47 +1,177 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    override fun onStart() {
+        super.onStart()
+        println("жц-стартует")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("жц-восст")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("жц-паузит")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("жц-уничтожает")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("жц-останавливает")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        println("жц-создает")
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyApplicationTheme{
+                ViewMain()
             }
         }
     }
-}
+    @Composable
+    fun ViewMain(){
+        var message by remember() { mutableStateOf("") }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+        ) {
+            Image(
+                painter = painterResource(R.drawable.misha),
+                contentDescription = null
+
+            )
+
+            TextField(
+                value = message,
+                onValueChange = { newText ->
+                    message = newText
+                },
+                textStyle = TextStyle(fontSize = 30.sp),
+                modifier = Modifier.padding(bottom = 25.dp)
+            )
+
+
+            Text(
+                text = message,
+                fontSize = 50.sp,
+                modifier = Modifier.padding(bottom = 45.dp)
+            )
+
+            Button(
+                onClick = {
+                    val toastMessage = if (message.isNotEmpty()) {
+                        "всем похуй но, вы написали: ${message}"
+                    } else {
+                        "101"
+                    }
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        toastMessage,
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+                }
+            ) {
+                Text(text = "хуячь", fontSize = 50.sp)
+            }
+        }
+
+
     }
+    @Preview(showBackground = true)
+    @Composable
+    fun ViewMainPreview(){
+        var message by remember() { mutableStateOf("")
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+
+        ) {
+            Image(
+                painter = painterResource(R.drawable.misha),
+                contentDescription = null
+
+            )
+
+            TextField(
+                value = message,
+                onValueChange = { newText ->
+                    message = newText
+                },
+                textStyle = TextStyle(fontSize = 30.sp),
+                modifier = Modifier.padding(bottom = 25.dp)
+            )
+
+
+            Text(
+                text = message,
+                fontSize = 50.sp,
+                modifier = Modifier.padding(bottom = 45.dp)
+            )
+
+            Button(
+                onClick = {
+                    val toastMessage = if (message.isNotEmpty()) {
+                        "всем похуй но, вы написали: ${message}"
+                    } else {
+                        "101"
+                    }
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        toastMessage,
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+                }
+            ) {
+                Text(text = "хуячь", fontSize = 50.sp)
+            }
+        }
+
+
+    }
+
+
+
 }
+
+
+
+
